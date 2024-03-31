a88b55d7eea8cb94e6b0de560b95d693a122550a
Egor Kurlenko <Egor Kurlenko@DESKTOP-RBCEALT>
2024-03-31 12:39:53 +0300

    Fixed

--- ConsoleApp1/RationalNumberAbstract.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RatioAbstract
-{
-    abstract class RationalNumberAbstract
-    {
-        public abstract int GCD(int numerator, int denominator);
-        public abstract void Reduse();
-    }
-}
